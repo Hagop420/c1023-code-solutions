4201458df314b81ad49d0510e1a14f46ea46e598
Hagop Almadjian <hagop1isaboss@hotmail.com>
2023-10-23 22:33:32 +0000

    Completed javascript-operations-and-expressions excersize

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,50 @@
+const width = 22;
+const height = 3;
+const area = width / height;
+
+console.log(`Value of area: ${area}`);
+console.log(`typeof area: ${typeof area}`);
+const bill = 200;
+const payment = 300;
+const change = payment - bill;
+
+console.log(`Value of change: ${change}`);
+console.log(`typeof change: ${typeof change}`);
+
+const quizzes = 12;
+const midterm = 21;
+const final = 33;
+
+const grade = quizzes / midterm / final;
+
+console.log(`Value of grade: ${grade}`);
+console.log(`typeof grade: ${typeof grade}`);
+
+const firstName = 'Jack';
+const lastName = 'Almadjian';
+
+const fullName = `${firstName} ${lastName}`;
+
+console.log(fullName);
+console.log(`typeof fullName: ${typeof fullName}`);
+
+const ph = 7;
+
+const isAcidic = ph < 7;
+
+console.log(`Value of isAcidic: ${isAcidic}`);
+console.log(`typeof isAcidic: ${typeof isAcidic}`);
+
+const headcount = 20;
+
+const isSparta = headcount === 300;
+
+console.log(`Value of isSparta: ${isSparta}`);
+console.log(typeof isSparta);
+
+let motto = fullName;
+motto = `${fullName} is the goat`;
+
+console.log(`Value of motto: ${motto}`);
+console.log(`typeof motto: ${typeof motto}`);
+//
