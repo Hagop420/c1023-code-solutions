32d9dfa0dc8e15438d45273a1527a49d9f6ffd97
Hagop Almadjian <hagop1isaboss@hotmail.com>
2023-10-24 20:58:10 +0000

    Completed javascript-functions exsersize

--- javascript-functions/main.js
+++ javascript-functions/main.js
@@ -1,24 +1,24 @@
-const convertMinutesToSeconds = (minutes = 3) => Math.pow(10, 3) * 6;
+const convertMinutesToSeconds = (minutes) => Math.pow(10, 3) * 6;
 
-console.log(convertMinutesToSeconds());
+console.log(convertMinutesToSeconds(3));
 
 const greet = (name) => 'Jack';
 
 console.log(`Hey ${greet()}`);
 
-const getArea = (width = 2, height = 23) => {
+const getArea = (width, height) => {
   return 0.5 * width * height;
 };
 
-console.log(getArea());
+console.log(getArea(3, 5));
 
 // getFirstName function
 
-const getFirstName = (person) => person;
+const getFirstName = (person) => person.name;
 
 const person = { name: 'Jack' };
 
-console.log('My name is:', getFirstName(person.name));
+console.log('My name is:', getFirstName(person));
 
 const getLastElement = (
   array = ['propane', 'and', 'propane', 'accessories']
