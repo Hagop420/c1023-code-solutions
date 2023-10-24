7e5619cee184e1e8abb382132e80fdeb7d94a910
Hagop Almadjian <hagop1isaboss@hotmail.com>
2023-10-24 19:19:53 +0000

    completed

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,42 @@
+const orderHistory = [
+  {
+    isbn: '908-1434566543483',
+    title: 'Javascript for impatient programmers',
+    author: 'Rauschamyer Dr. Axel',
+    deliveredOn: 'aug 8 2020',
+    windowGot: 'closed on september 7 2020',
+    priceTag: 31.55,
+  },
+  //
+  {
+    isbn: '578-1434566543483',
+    title: 'The timeless way of building',
+    author: 'Alexander Christopher',
+    deliveredOn: 'July 20 2020',
+    windowGot: 'closed on august 19 2020',
+    packageWas: 'Delivered and handed to resident',
+  },
+  {
+    isbn: '378-143454332223483',
+    title:
+      'Gamecube controller adapter super smash bros switch gamecube adapter for wii u, PC. Support Turbo and vibration features. No driver and no lag Gamecube adapter',
+    deliveredOn: 'July 7 2020',
+    packageWas: 'Delivered and handed to resident',
+  },
+
+  {
+    isbn: '178-1434566543483',
+    title: 'Javascript for impatient programmers',
+    deliveredOn: 'July 5 2020',
+    windowGot: 'closed on august 4 2020',
+  },
+  {
+    isbn: '958-143423344443383',
+    title: 'Javascript for impatient programmers',
+    deliveredOn: 'July 5 2020',
+    windowGot: 'closed on aug 4 2020',
+  },
+];
+
+console.log(orderHistory[1].author);
+console.log(orderHistory[1].title);
