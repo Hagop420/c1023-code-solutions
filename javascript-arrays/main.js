a8b358a4bdfb99c729c8482e32f8a3f993a86885
Hagop Almadjian <hagop1isaboss@hotmail.com>
2023-10-24 01:09:14 +0000

    Completed javascript-arrays excersize

---
+++ javascript-arrays/main.js
@@ -0,0 +1,23 @@
+const colors = ['red', 'white', 'blue'];
+
+console.log(`Value of colors[0]: ${colors[0]}`);
+console.log(`Value of colors[1]: ${colors[1]}`);
+console.log(`Value of colors[2]: ${colors[2]}`);
+
+// American flag colors
+
+console.log(`America is ${colors[0]}, ${colors[1]}, and ${colors[2]}`);
+colors[2] = 'green';
+console.log(`Mexico is ${colors[0]}, ${colors[1]}, and ${colors[2]}`);
+
+const students = ['Leo', 'Mike', 'randomName'];
+
+const numberOfStudents = students.length;
+
+console.log(`There are ${numberOfStudents} in the class`);
+
+console.log(
+  `The last student in the array is ${students[students.length - 1]}`
+);
+
+console.log('Value of students:', students);
