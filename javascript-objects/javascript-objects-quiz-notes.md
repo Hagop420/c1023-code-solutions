fe8b34416016db5c2176bf557bfc79c00175ac90
Hagop Almadjian <hagop1isaboss@hotmail.com>
2023-10-24 00:31:02 +0000

    Completed javascript-objects excersize

---
+++ javascript-objects/javascript-objects-quiz-notes.md
@@ -0,0 +1,47 @@
+# javascript-objects-quiz-notes
+
+## Quiz Questions
+
+After completing this exercise, you should be able to discuss or answer the following questions:
+
+- What are objects used for?
+  -Objects are used for storing multiple datatypes in a single variable
+- What are object properties?
+  -properties are the use of diffrent operations ans functionallities that are included with specific datatypes
+- Describe object literal notation.
+  -const person = {
+    <!-- any value types are parsed here -->
+  }
+  You can use a const var or a let
+- How do you remove a property from an object?
+  -By using the delete property that come with objects
+- What are the two ways to get or update the value of a property?
+  -Use of either the dot or the bracket notation
+
+## Notes
+
+All student notes should be written here.
+
+How to write `Code Examples` in markdown
+
+for JS:
+
+```javascript
+const data = 'Howdy';
+```
+
+for HTML:
+
+```html
+<div>
+  <p>This is text content</p>
+</div>
+```
+
+for CSS:
+
+```css
+div {
+  width: 100%;
+}
+```
---
+++ javascript-objects/main.js
@@ -0,0 +1,56 @@
+const student = {
+  firstName: 'Jack',
+  lastName: 'Almadjian',
+  age: 22,
+};
+
+const fullName = `${student.firstName} ${student.lastName} ${student.age}`;
+
+console.log(`Value of fullName: ${fullName}`);
+
+student.livesInIrvine = false;
+
+console.log(`Value of student.livesInIrvine: ${student.livesInIrvine}`);
+student.previousOccupation = 'Assistant Mechanic';
+
+console.log(student.livesInIrvine);
+
+console.log('Value of student:', student);
+
+console.log(
+  `Value of student.previousOccupation: ${student.previousOccupation}`
+);
+
+// vehicle object creation
+
+const vehicle = {
+  make: 'Bmw',
+  model: '440i',
+  year: 2017,
+};
+
+vehicle.color = 'black';
+
+vehicle.isConvertible = false;
+
+console.log(`Value of vehicle.["color"]: ${vehicle.color}`);
+console.log(`Value of vehicle["isConvertible"]: ${vehicle.isConvertible}`);
+
+// entire vehicle object enter printing to console log
+
+console.log('Value of vehicle:', vehicle);
+
+// pet object
+
+const pet = {
+  name: 'Jolie',
+  type: 'Lab',
+};
+
+delete pet.name;
+
+delete pet.type;
+
+// logging now the empty object cause delete has been parsed in code
+
+console.log('Value of pet:', pet);
