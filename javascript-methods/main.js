b0affd2be02943709fce716d51bf2048e299a26b
Hagop Almadjian <hagop1isaboss@hotmail.com>
2023-10-25 16:35:32 +0000

    Completed

--- javascript-methods/main.js
+++ javascript-methods/main.js
@@ -1,21 +1,7 @@
-const x = 10;
-const y = 8;
-const z = 7;
+// const x = 10;
+// const y = 8;
+// const z = 7;
 
-const maximumValue = Math.max(x, y, z);
+// const maximumValue = Math.max(x , y , z)
 
-console.log(maximumValue);
-
-const heroes = ['mike', 'Parth', 'Jack'];
-
-let randomNumber = Math.random();
-
-randomNumber *= heroes.length;
-
-const randomIndex = Math.floor(randomNumber);
-
-console.log(randomIndex);
-
-const randomHero = heroes[randomIndex];
-
-console.log(randomHero);
+// const heroes =
