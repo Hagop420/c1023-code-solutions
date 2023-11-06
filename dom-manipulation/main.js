02dd5a5ff1c7103d8ef1d7add3aa9c774d085a76
Hagop Almadjian <hagop1isaboss@hotmail.com>
2023-11-06 23:51:55 +0000

    Completed-js-dom-Manipulation-excersize

--- dom-manipulation/main.js
+++ dom-manipulation/main.js
@@ -2,25 +2,25 @@ const btn = document.querySelector('.hot-button');
 
 let counter = 0;
 
-const mainBtn = btn;
-
 const countDisplay = document.querySelector('.click-count');
 
-mainBtn.addEventListener('click', (e) => {
+btn.addEventListener('click', (e) => {
   counter += 1;
   countDisplay.innerHTML = `Clicks: ${counter}`;
 
   if (counter <= 4) {
     btn.className = 'hot-button cold';
-  } else if (counter <= 7) {
+  } else if (counter < 7) {
     btn.className = 'hot-button cool';
-  } else if (counter <= 10) {
+  } else if (counter < 10) {
     btn.className = 'hot-button tepid';
-  } else if (counter <= 13) {
+  } else if (counter < 13) {
     btn.className = 'hot-button warm';
-  } else if (counter <= 16) {
+  } else if (counter < 16) {
     btn.className = 'hot-button hot';
   }
 
   console.log(e.target);
 });
+
+btn.textContent = 'Cold button';
