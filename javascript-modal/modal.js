dab8d5806342f025690cb982f135db9539f5b8eb
Hagop Almadjian <hagop1isaboss@hotmail.com>
2023-11-07 22:32:29 +0000

    Completed-js-modals

--- javascript-modal/modal.js
+++ javascript-modal/modal.js
@@ -1,5 +1,16 @@
-const workingWithModals = document.querySelector('#modal');
+const modal = document.querySelector('.modal-container');
+const modalBtnOpener = document.querySelector('#modal-btn');
+const modalBtn = document.querySelector('.modal-content button');
 
-workingWithModals.addEventListener('click', () => {
-  document.body.className = 'kinda_black';
+modal.className = 'none';
+
+modalBtnOpener.addEventListener('click', () => {
+  modal.className = 'modal-container open_Modal';
+  modalBtnOpener.className = 'op';
+  console.log(9);
+});
+
+modalBtn.addEventListener('click', () => {
+  modal.className = 'nne';
+  modalBtnOpener.className = 'op-one';
 });
