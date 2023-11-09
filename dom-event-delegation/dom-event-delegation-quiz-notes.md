51dab992eab5afb8ae3663c4be1039f5df3b4dc1
Hagop Almadjian <hagop1isaboss@hotmail.com>
2023-11-09 22:27:58 +0000

    Completed-dom-delegation-excersize

--- dom-event-delegation/dom-event-delegation-quiz-notes.md
+++ dom-event-delegation/dom-event-delegation-quiz-notes.md
@@ -5,11 +5,12 @@
 After completing this exercise, you should be able to discuss or answer the following questions:
 
 - What is the `event.target`?
-
+  -event.target is js code that parses the event firing handler type to the console with a console.log()
 - Why is it possible to listen for events on one element that actually happen its descendent elements?
+  -If they all are inside the same parent container it will be valid
 
 - What DOM element property tells you what type of element it is?
-
+  -event.type will tell you
 - What does the `element.closest()` method take as its argument and what does it return?
 
 - How can you remove an element from the DOM?
--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -1,16 +1,13 @@
 const taskEvent = document.querySelector('.task-list');
 
 taskEvent.addEventListener('click', (e) => {
-  console.log('event.target:', e.target);
+  // console.log('event.target:', e.target);
   console.log(`e.target.tagName: ${e.target.tagName}`);
 
-  const btnAll = document.querySelectorAll('button');
-  btnAll.forEach((btn) => {
+  // console.log(e.target.nodeName)
+
+  if (e.target.tagName === 'BUTTON') {
     const cl = e.target.closest('.task-list-item');
-    if (e.target === btn) {
-      btn.previousElementSibling.parentNode.remove();
-      // btn.remove()
-      console.log('Closest item:', cl);
-    }
-  });
+    cl.remove();
+  }
 });
