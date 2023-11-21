542623aab1308ecf2f75d1a6effa205dae8e546e
Hagop Almadjian <hagop1isaboss@hotmail.com>
2023-11-21 18:45:01 +0000

    JS new keyword

---
+++ javascript-new/new-objects.js
@@ -0,0 +1,41 @@
+// creating an object
+
+const anObject = new Object();
+console.log('Value of an object:', anObject);
+
+console.log('typeof anObject:', typeof anObject);
+
+// creating an array
+
+const anArray = new Array();
+console.log('Value of an object:', anArray);
+
+console.log(`typeof anArray: ${typeof anArray}`);
+
+// creating an array
+
+const aFunction = new Function();
+
+console.log(`Value of aFunction: ${aFunction}`);
+console.log(`typeof aFunction: ${typeof aFunction}`);
+
+// creating a number
+
+const aNumber = new Number();
+console.log('Value of an object:', aNumber);
+
+console.log(`typeof aNumber: ${typeof aNumber}`);
+
+// creating a boolean true/False
+
+const aBool = new Boolean();
+console.log('Value of an object:', aBool);
+
+console.log(`typeof aBool: ${typeof aBool}`);
+
+// creating a string
+
+const aStr = new String();
+console.log('Value of an object:', aStr);
+
+console.log(`typeof aStr: ${typeof aStr}`);
