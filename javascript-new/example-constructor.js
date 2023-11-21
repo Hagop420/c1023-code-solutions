dd0f3745a194913c06ea7337ef2552b327311f28
Hagop Almadjian <hagop1isaboss@hotmail.com>
2023-11-21 19:38:16 +0000

    new-keyword

---
+++ javascript-new/example-constructor.js
@@ -0,0 +1,15 @@
+// function using the new keyword ExampleConstructor
+
+function ExampleConstructor(one) {}
+
+console.log(ExampleConstructor.prototype);
+console.log(`${typeof ExampleConstructor.prototype}`);
+
+// calling the func with a new as an arg
+
+const newKeywordArg = new ExampleConstructor();
+
+console.log(
+  'The typeof in the function with a new argument is a:',
+  newKeywordArg
+);
