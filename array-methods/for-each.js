2a7b4f46e29b14610013927d69ca5a21437b4d2d
Hagop Almadjian <hagop1isaboss@hotmail.com>
2023-12-05 06:55:20 +0000

    done

--- array-methods/for-each.js
+++ array-methods/for-each.js
@@ -1,9 +1,16 @@
 const values = [10, 1, 22, 23, 41, 5, 18, 7, 80, 9];
 
-values.forEach((vals) => {
-  console.log('In order:', vals);
-});
+// values.forEach((vals) => {
+//   console.log('In order:', vals);
+// });
 
-values.reverse('').forEach((vals) => {
-  console.log('Reverse order:', vals);
+// values.reverse('').forEach((vals) => {
+//   console.lo5g('Reverse order:', vals);
+// });
+
+const length = values.length;
+
+values.forEach((_, ind) => {
+  const reverseIndex = length - 1 - ind;
+  console.log('in reverse:', values[reverseIndex]);
 });
