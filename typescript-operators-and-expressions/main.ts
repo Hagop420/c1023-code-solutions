a43c1cc81a6b33cbbf1d8719391c0f66aac4935d
Hagop Almadjian <hagop1isaboss@hotmail.com>
2023-12-06 23:27:01 +0000

    completed TSX practice

---
+++ typescript-operators-and-expressions/main.ts
@@ -0,0 +1,71 @@
+// 1st problem with width/height
+
+const width: number = 30;
+
+const height: number = 20;
+
+const area: number = width * height;
+
+console.log('Value of area:', area);
+console.log('typeof area:', typeof area);
+
+// 2nd problem
+
+const bill: number = 800;
+const payment: number = 344;
+
+const change: number = bill - payment;
+
+console.log('Value of change:', change);
+console.log('typeof change:', typeof change);
+
+// 3rd problem
+
+const quizzes: number = 100;
+const midterm: number = 70;
+const final: number = 50;
+
+// 4th solution
+
+const grade: number = (quizzes * midterm * final) / 3;
+
+console.log('value of grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+// 5th solution
+
+const firstName: string = 'Jack';
+
+const lastName: string = 'Almadjian';
+
+const fullName: string = `${firstName} ${lastName}`;
+
+console.log(`My full name is ${fullName}`);
+
+console.log(`typeof fullName is a: ${typeof fullName}`);
+
+// 6th solution
+
+const pH: number = 5;
+
+const isAcidic: boolean = pH < 7;
+
+console.log(`Value of isAcidic ${isAcidic}`);
+
+console.log(`typeof isAcidic ${typeof isAcidic}`);
+
+// 7th solution
+const headCount: number = 30;
+
+const isSparta: boolean = headCount === 300;
+
+console.log(`Value of isSparta ${isSparta}`);
+
+console.log(`typeof isSparta ${typeof isSparta}`);
+
+// final solution
+const motto: string = fullName + ' is the 🐐';
+
+console.log(`Value of motto ${motto}`);
+
+console.log(`typeof motto ${typeof motto}`);
---
+++ typescript-operators-and-expressions/typescript-operators-and-expressions-quiz-notes.md
@@ -0,0 +1,44 @@
+# javascript-operators-and-expressions-quiz-notes
+
+## Quiz Questions
+
+After completing this exercise, you should be able to discuss or answer the following questions:
+
+- What data type is returned by an arithmetic operation?
+- A number is returned from an arethmetic operation
+- What is string concatenation?
+- String concatanation is the way of adding on to a variable or a string
+- What purpose(s) does the `+` plus operator serve in JavaScript?
+- the plus operator adds two or more numbers together and can also serve as a way of concatanating strings together
+- What data type is returned by comparing two values (`<`, `>`, `===`, etc)?
+- Boolean
+- What does the `+=` "plus-equals" operator do?
+- Appends strings together kind of like concatanation
+
+## Notes
+
+All student notes should be written here.
+
+How to write `Code Examples` in markdown
+
+for JS:
+
+```javascript
+const data = 'Howdy';
+```
+
+for HTML:
+
+```html
+<div>
+  <p>This is text content</p>
+</div>
+```
+
+for CSS:
+
+```css
+div {
+  width: 100%;
+}
+```
