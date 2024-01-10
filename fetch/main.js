04c1dd17023f03c4081c4c690ec3d9794011104e
Hagop Almadjian <hagop1isaboss@hotmail.com>
2024-01-10 19:50:51 +0000

    Commited-fetch-method-which-in-a-shorthand-way-of-doing-XMLreq

--- fetch/main.js
+++ fetch/main.js
@@ -1,8 +1,15 @@
 // Fetching in a sample placeholder API that get's the name's converted to JSON format
 const getAsyncAPIDataFromDataBase = async () => {
-  const response = await fetch('https://jsonplaceholder.typicode.com/users');
-  const jsonPlaced = await response.json();
-  console.log(jsonPlaced);
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const jsonPlaced = await response.json();
+    if (!response.ok) {
+      throw new error(`Fetch error ${response.status}`);
+    }
+    console.log(jsonPlaced);
+  } catch (err) {
+    console.log('ERR of:', err);
+  }
 };
 
 getAsyncAPIDataFromDataBase();
@@ -11,9 +18,16 @@ getAsyncAPIDataFromDataBase();
 // Getting/Recieving a GET request that get's my favorite pokemon from pokeAPI
 
 const pokeAPIGetReq = async () => {
-  const response = await fetch('https://pokeapi.co/api/v2/pokemon/1');
-  const jsonPlaced = await response.json();
-  console.log(jsonPlaced);
+  try {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon/1');
+    if (!response.ok) {
+      throw new error(`Fetch error ${respone.status}`);
+    }
+    const jsonPlaced = await response.json();
+    console.log(jsonPlaced);
+  } catch (err) {
+    console.log('ERR of:', err);
+  }
 };
 
 pokeAPIGetReq();
